830bf6a331ed335c947a7310ff8ec33807ea7f20
Sanskritforum <Sanskritforum@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-10-12 01:57:17 +0000

    Version 2.4 commit - Added BuddyPress support
    git-svn-id: https://plugins.svn.wordpress.org/skt-nurcaptcha/tags/2.4@450092 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ uninstall.php
@@ -0,0 +1,9 @@
+<?php 
+if(!defined('ABSPATH') && !defined('WP_UNINSTALL_PLUGIN') ) die();
+delete_option('sktnurc_privtkey');
+delete_option('sktnurc_publkey');
+delete_option('sktnurc_lang');
+delete_option('sktnurc_theme');
+delete_option('sktnurc_count');
+delete_option('sktnurc_keysset');
+?>
